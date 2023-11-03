cdd86b331b6b3c1e408680ef0ad6297fda793ddd
Dima <uvarov3123@gmail.com>
2023-11-03 10:44:28 +0200

    add hero

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -8,7 +8,7 @@
 // При необхідності підключаємо додаткові модулі слайдера, вказуючи їх у {} через кому
 // Приклад: { Navigation, Autoplay }
 import Swiper from 'swiper';
-import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, EffectFade,  Pagination } from 'swiper/modules';
 /*
 Основні модулі слайдера:
 Navigation, Pagination, Autoplay,
@@ -26,85 +26,6 @@ import "../../scss/base/swiper.scss";
 
 // Ініціалізація слайдерів
 function initSliders() {
-	// Список слайдерів
-	// Перевіряємо, чи є слайдер на сторінці
-	if (document.querySelector('.dsf1234342')) {
-			new Swiper('.df12324234', {
-			modules: [Navigation],
-			observer: true,
-			observeParents: true,
-			slidesPerView: 1,
-			spaceBetween: 0,
-			//autoHeight: true,
-			speed: 800,
-
-			// lazyPreloaderClass: 'preloader',
-
-			//touchRatio: 0,
-			//simulateTouch: false,
-			//loop: true,
-			//preloadImages: false,
-			//lazy: true,
-
-			/*
-			// Ефекти
-			effect: 'fade',
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-			},
-			*/
-
-			// Пагінація
-			/*
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			*/
-
-			// Скроллбар
-			/*
-			scrollbar: {
-				el: '.swiper-scrollbar',
-				draggable: true,
-			},
-			*/
-
-			// Кнопки "вліво/вправо"
-			navigation: {
-				prevEl: '.swiper-button-prev',
-				nextEl: '.swiper-button-next',
-			},
-			/*
-			// Брейкпоінти
-			breakpoints: {
-				640: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
-				},
-				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
-				},
-				992: {
-					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
-				},
-			},
-			*/
-			// Події
-			on: {
-
-			}
-		});
-	}
-
 
 	if (document.querySelector('.sreen__slider')) {
 		new Swiper('.sreen__slider', {
@@ -133,62 +54,65 @@ function initSliders() {
 	});
 
 
-	for (const mobileSlider of document.querySelectorAll('.partners__items')) {
-        if (mobileSlider) {
-            (function () {
-                "use strict";
 
-                const breakpoint = window.matchMedia("(min-width:768px)");
-                let slider;
+}
 
-                const enableSwiper = function () {
-                    slider = new Swiper(mobileSlider, {
-                        modules: [Autoplay],
-						observer: true,
-						observeParents: true,
-						slidesPerView: "auto",
-						spaceBetween: 43,
 
-						speed: 2000,
-						loop: true,
-						loopAdditionalSlides: 5,
-						allowTouchMove: false,
+for (const mobileSlider of document.querySelectorAll('.partners__items')) {
+	if (mobileSlider) {
+		(function () {
+			"use strict";
 
-						autoplay: {
-							delay: 0,
-							disableOnInteraction: false,
+			const breakpoint = window.matchMedia("(min-width:768px)");
+			let slider;
+
+			const enableSwiper = function () {
+				slider = new Swiper(mobileSlider, {
+					modules: [Autoplay],
+					observer: true,
+					observeParents: true,
+					slidesPerView: "auto",
+					spaceBetween: 43,
+
+					speed: 2000,
+					loop: true,
+					loopAdditionalSlides: 5,
+					allowTouchMove: false,
+
+					autoplay: {
+						delay: 0,
+						disableOnInteraction: false,
+					},
+					breakpoints: {
+						320: {
+							spaceBetween: 25,
 						},
-						breakpoints: {
-							320: {
-								spaceBetween: 25,
-							},
-							768: {
-								spaceBetween: 30,
-							},
-							992: {
-								spaceBetween: 43,
-							},
+						768: {
+							spaceBetween: 30,
 						},
+						992: {
+							spaceBetween: 43,
+						},
+					},
 
-                    });
-                };
+				});
+			};
 
-                const breakpointChecker = function () {
-                    if (breakpoint.matches === true) {
-                        if (slider !== undefined) slider.destroy(true, true);
+			const breakpointChecker = function () {
+				if (breakpoint.matches === true) {
+					if (slider !== undefined) slider.destroy(true, true);
 
-                        return;
-                    } else if (breakpoint.matches === false) {
-                        return enableSwiper();
-                    }
-                };
+					return;
+				} else if (breakpoint.matches === false) {
+					return enableSwiper();
+				}
+			};
 
-                breakpoint.addListener(breakpointChecker);
-                breakpointChecker();
-            })();
-        }
+			breakpoint.addListener(breakpointChecker);
+			breakpointChecker();
+		})();
+	}
 
-    }
 }
 
 }
